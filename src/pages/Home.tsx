1a5a8020f628dec5c95434afe9dfab26906ecc81
Aeliondw <aeliondw@gmail.com>
2024-10-03 00:04:49 +0200

    [+] feature(frontend): add auth linked with server api

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Page, user is authenticated</h1>
+        </div>
+    );
+};  
+
+export default Home;
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -3,35 +3,27 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import { RootState } from "../../redux/store";
-import { useLoginMutation } from "../../redux/api/authApi";
 
 import MetaData from "../../components/layout/MetaData";
-import AuthForm from "../../components/auth/AuthForm";
-import { Eclipse } from "lucide-react";
+import TabButton from "../../components/auth/TabButton";
+import LoginForm from "../../components/auth/LoginForm";
+import RegisterForm from "../../components/auth/RegisterForm";
 
 const Login: React.FC = () => {
-    const [username, setUsername] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
+    const [activeTab, setActiveTab] = useState<string>("");
 
     const navigate = useNavigate();
 
-    const [login, { isLoading, data }] = useLoginMutation();
     const { isAuthenticated } = useSelector((state: RootState) => state.auth);
+
+    const handleTabClick = (label: string) => {
+        setActiveTab(label);
+    }
 
     useEffect(() => {
         if (isAuthenticated)
-// TODO: Change to /home (EPSEED profile)
-            navigate("/");
+            navigate("/home");
     }, [isAuthenticated, navigate]);
-
-    const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const loginData = {
-            username,
-            password,
-        };
-        login(loginData);
-    };
 
 // TODO: grid responsive
     return (
@@ -39,13 +31,33 @@ const Login: React.FC = () => {
             <MetaData title="Sign in" />
             <div className="grid grid-cols-2 w-full mt-40 items-start justify-center min-h-screen">
                 <div className="flex flex-col col-start-1">
-                    <div className="homepage-h1 text-8xl font-black">Espeen</div>
-                    <div className="homepage-h2 font-bold">Spin actions into reactions !</div>
+                    <div className="homepage-h1 text-8xl font-black">
+                        Espeen
+                    </div>
+                    <div className="homepage-h2 font-bold">
+                        Spin actions into reactions !
+                    </div>
                 </div>
-                <div className="flex flex-col col-start-2">
-                    <AuthForm />
+
+{/* TODO: Fix TabButton mb */}
+                <div className="flex flex-col col-start-2 items-center">
+                    <div className="flex w-[500px]">
+                        <TabButton
+                            label="Login"
+                            isActive={activeTab === "Login"}
+                            onClick={() => handleTabClick("Login")}
+
+                        />
+                        <TabButton
+                            label="Sign Up"
+                            isActive={activeTab === "Sign Up"}
+                            onClick={() => handleTabClick("Sign Up")}
+                        />
+                    </div >
+                    {activeTab === "Login" ? <LoginForm /> : <RegisterForm />}
                 </div>
-            </div>
+
+            </div>  
         </>
     );
 };
