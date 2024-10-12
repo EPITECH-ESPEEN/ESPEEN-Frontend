5ce4fb514112056994c236750a82f83d54ef859a
Dan13615 <daniil.stepanov@epitech.eu>
2024-10-13 00:59:57 +0200

    [+] google OAuth2 tests
    Co-authored-by: Tech0ne <Tech0ne@users.noreply.github.com>
    Co-authored-by: Aeliondw <Aeliondw@users.noreply.github.com>
    Co-authored-by: dragusheen <dragusheen@users.noreply.github.com>

--- src/components/card/serviceCard.tsx
+++ src/components/card/serviceCard.tsx
@@ -15,39 +15,63 @@ import { useTranslation } from "react-i18next";
 import { IService, IServiceButton } from "../../types/Services";
 import css from "./serviceCard.module.css";
 import ColoredButton from "../buttons/colored/coloredButton";
-
+import { useGetGoogleAuthQuery } from "../../redux/api/googleServiceApi";
 
 /* ----- PROPS ----- */
 interface ServiceCardProps {
-    service: IService;
+  service: IService;
 }
 
 /* ----- COMPONENT ----- */
 const ServiceCard: React.FC<ServiceCardProps> = ({ service }) => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const callPath = (action: IServiceButton) => {
-        console.log(`${action.name} : ${action.path}`);
+  const { data: googleAuthData } = useGetGoogleAuthQuery({});
+
+  //   const callPath = async (action: IServiceButton) => {
+  //     // axios -> call /api/auth
+  //     // window.location.href = `${process.env.BACKEND_HOST}/${action.path}`;
+
+  //     const response = await axios.get("http://localhost:4242/api/auth");
+  //     const { authUrl } = response.data;
+  //     console.log(authUrl);
+
+  //     if (authUrl) {
+  //       window.location.href = authUrl;
+  //     }
+  //     navigate(action.path);
+
+  const callPath = (action: IServiceButton) => {
+    if (action.name !== "not_linked") {
+      window.location.href = action.path;
+      //   //   if (googleAuthData?.url) {
+      //   //     console.log("Hook");
+      //   //     window.location.href = googleAuthData.url;
+      //   //   } else {
+      //   //     console.log("Hello world");
+      //   //   }
+      // } else {
+      //   console.log(`${action.name} : ${action.path}`);
     }
+  };
 
-    return (
-        <div className={css.container}>
-            <img className={css.img} src={service.icon} alt="service icon" />
-            <div className={css.textsContainer}>
-                <div>
-                    <div className="textStyle-text color-dark">{ t('dico.name') }</div>
-                    <div className="textStyle-title color-dark">{service.name}</div>
-                </div>
-                <div className={css.buttonContainer}>
-                    <div className="textStyle-text color-dark">{ t('dico.actions') }</div>
-                    { service.buttons.map((action, index) => (
-                        <ColoredButton key={index} label={t(`services.${action.name}`)} onClick={() => callPath(action)} color={action.name === "linked" ? "green" : action.name === "not_linked" ? "red" : "dark"} />
-                    ))}
-                </div>
-            </div>
+  return (
+    <div className={css.container}>
+      <img className={css.img} src={service.icon} alt="service icon" />
+      <div className={css.textsContainer}>
+        <div>
+          <div className="textStyle-text color-dark">{t("dico.name")}</div>
+          <div className="textStyle-title color-dark">{service.name}</div>
         </div>
-    );
+        <div className={css.buttonContainer}>
+          <div className="textStyle-text color-dark">{t("dico.actions")}</div>
+          {service.buttons.map((action, index) => (
+            <ColoredButton key={index} label={t(`services.${action.name}`)} onClick={() => callPath(action)} color={action.name === "linked" ? "green" : action.name === "not_linked" ? "red" : "dark"} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceCard;
-
--- src/store/Services.ts
+++ src/store/Services.ts
@@ -9,8 +9,7 @@
 */
 
 /* ----- IMPORTS ----- */
-import { IService } from '../types/Services';
-
+import { IService } from "../types/Services";
 
 /* ----- DATAS ----- */
 let lastFetch: number = 0;
@@ -18,329 +17,281 @@ const services: Map<number, IService> = new Map();
 
 // TODO: Remove this when the fetch is working
 const tmp: IService[] = [
-    {
-        service_id: 1,
-        name: "Github",
-        status: false,
-        icon: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
-        buttons: [
-            {
-                name: "not_linked",
-                path: ""
-            },
-        ],
-        actions: [
-        ],
-        reactions: [
-        ],
-    },
-    {
-        service_id: 2,
-        name: "Discord",
-        status: true,
-        icon: "https://support.discord.com/hc/user_images/Tb2e5KLD1N4L_3PFVZ2Uuw.png",
-        buttons: [
-            {
-                name: "linked",
-                path: ""
-            },
-            {
-                name: "add_bot",
-                path: ""
-            },
-        ],
-        actions: [
-            {
-                action_id: 1,
-                name: "receive_private_message",
-            },
-            {
-                action_id: 2,
-                name: "receive_friend_request",
-            },
-        ],
-        reactions: [
-            {
-                reaction_id: 1,
-                name: "send_private_message",
-            },
-            {
-                reaction_id: 2,
-                name: "send_friend_request",
-            },
-        ],
-    },
-    {
-        service_id: 3,
-        name: "Spotify",
-        status: true,
-        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcDq7YmOwPrUEXB1gaGgWmgMYsqdYrtFu4sA&s",
-        buttons: [
-            {
-                name: "linked",
-                path: "",
-            },
-        ],
-        actions: [
-            {
-                action_id: 3,
-                name: "song_started",
-            },
-        ],
-        reactions: [
-        ],
-    },
-    {
-        service_id: 4,
-        name: "Twitch",
-        status: true,
-        icon: "https://image.similarpng.com/very-thumbnail/2021/01/Twitch-logo-on-transparent-background-PNG.png",
-        buttons: [
-            {
-                name: "linked",
-                path: "",
-            },
-        ],
-        actions: [
-            {
-                action_id: 3,
-                name: "live_started",
-            },
-            {
-                action_id: 4,
-                name: "live_ended",
-            },
-        ],
-        reactions: [
-            {
-                reaction_id: 3,
-                name: "send_chat_message",
-            },
-            {
-                reaction_id: 4,
-                name: "send_notification",
-            },
-            {
-                reaction_id: 5,
-                name: "send_donation",
-            }
-        ],
-    },
-    {
-        service_id: 5,
-        name: "Twitter",
-        status: false,
-        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/1245px-Logo_of_Twitter.svg.png",
-        buttons: [
-            {
-                name: "not_linked",
-                path: "",
-            },
-        ],
-        actions: [
-        ],
-        reactions: [
-        ],
-    },
-    {
-        service_id: 6,
-        name: "Youtube",
-        status: true,
-        icon: "https://img.freepik.com/vecteurs-premium/logo-youtube-rouge-logo-medias-sociaux_197792-1803.jpg?w=360",
-        buttons: [
-            {
-                name: "linked",
-                path: "",
-            },
-        ],
-        actions: [
-            {
-                action_id: 5,
-                name: "video_uploaded",
-            },
-            {
-                action_id: 6,
-                name: "video_liked",
-            }
-        ],
-        reactions: [
-            {
-                reaction_id: 6,
-                name: "comment_video",
-            },
-            {
-                reaction_id: 7,
-                name: "like_video",
-            },
-            {
-                reaction_id: 8,
-                name: "share_video",
-            }
-        ],
-    },
-    {
-        service_id: 7,
-        name: "Reddit",
-        status: false,
-        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ0neIueoYcxj62azhnFSpTxZFdQMcITEjCw&s",
-        buttons: [
-            {
-                name: "not_linked",
-                path: "",
-            },
-        ],
-        actions: [
-        ],
-        reactions: [
-        ],
-    },
-    {
-        service_id: 8,
-        name: "Google",
-        status: true,
-        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png",
-        buttons: [
-            {
-                name: "linked",
-                path: "",
-            },
-        ],
-        actions: [
-            {
-                action_id: 7,
-                name: "receive_email",
-            },
-            {
-                action_id: 8,
-                name: "receive_calendar_event",
-            },
-            {
-                action_id: 9,
-                name: "receive_contact_request",
-            },
-            {
-                action_id: 10,
-                name: "receive_document",
-            }
-        ],
-        reactions: [
-            {
-                reaction_id: 9,
-                name: "send_email",
-            },
-            {
-                reaction_id: 10,
-                name: "send_calendar_event",
-            },
-            {
-                reaction_id: 11,
-                name: "send_contact_request",
-            },
-            {
-                reaction_id: 12,
-                name: "send_document",
-            }
-        ],
-    },
-    {
-        service_id: 9,
-        name: "Facebook",
-        status: false,
-        icon: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Facebook_logo_%28square%29.png",
-        buttons: [
-            {
-                name: "not_linked",
-                path: "",
-            },
-        ],
-        actions: [
-        ],
-        reactions: [
-        ],
-    },
-    {
-        service_id: 10,
-        name: "Instagram",
-        status: true,
-        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/768px-Instagram_icon.png",
-        buttons: [
-            {
-                name: "linked",
-                path: "",
-            },
-        ],
-        actions: [
-            {
-                action_id: 11,
-                name: "receive_private_message",
-            },
-            {
-                action_id: 12,
-                name: "receive_friend_request",
-            },
-            {
-                action_id: 13,
-                name: "post_like",
-            },
-            {
-                action_id: 14,
-                name: "story_seen",
-            }
-        ],
-        reactions: [
-            {
-                reaction_id: 13,
-                name: "send_private_message",
-            },
-            {
-                reaction_id: 14,
-                name: "send_friend_request",
-            },
-            {
-                reaction_id: 15,
-                name: "like_post",
-            },
-            {
-                reaction_id: 16,
-                name: "share_story",
-            }
-        ],
-    },
+  {
+    service_id: 1,
+    name: "Github",
+    status: false,
+    icon: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+    buttons: [
+      {
+        name: "not_linked",
+        path: "",
+      },
+    ],
+    actions: [],
+    reactions: [],
+  },
+  {
+    service_id: 2,
+    name: "Discord",
+    status: true,
+    icon: "https://support.discord.com/hc/user_images/Tb2e5KLD1N4L_3PFVZ2Uuw.png",
+    buttons: [
+      {
+        name: "linked",
+        path: "",
+      },
+      {
+        name: "add_bot",
+        path: "",
+      },
+    ],
+    actions: [
+      {
+        action_id: 1,
+        name: "receive_private_message",
+      },
+      {
+        action_id: 2,
+        name: "receive_friend_request",
+      },
+    ],
+    reactions: [
+      {
+        reaction_id: 1,
+        name: "send_private_message",
+      },
+      {
+        reaction_id: 2,
+        name: "send_friend_request",
+      },
+    ],
+  },
+  {
+    service_id: 3,
+    name: "Spotify",
+    status: true,
+    icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcDq7YmOwPrUEXB1gaGgWmgMYsqdYrtFu4sA&s",
+    buttons: [
+      {
+        name: "linked",
+        path: "",
+      },
+    ],
+    actions: [
+      {
+        action_id: 3,
+        name: "song_started",
+      },
+    ],
+    reactions: [],
+  },
+  {
+    service_id: 4,
+    name: "Twitch",
+    status: true,
+    icon: "https://image.similarpng.com/very-thumbnail/2021/01/Twitch-logo-on-transparent-background-PNG.png",
+    buttons: [
+      {
+        name: "linked",
+        path: "",
+      },
+    ],
+    actions: [
+      {
+        action_id: 3,
+        name: "live_started",
+      },
+      {
+        action_id: 4,
+        name: "live_ended",
+      },
+    ],
+    reactions: [
+      {
+        reaction_id: 3,
+        name: "send_chat_message",
+      },
+      {
+        reaction_id: 4,
+        name: "send_notification",
+      },
+      {
+        reaction_id: 5,
+        name: "send_donation",
+      },
+    ],
+  },
+  {
+    service_id: 5,
+    name: "Twitter",
+    status: false,
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/1245px-Logo_of_Twitter.svg.png",
+    buttons: [
+      {
+        name: "not_linked",
+        path: "",
+      },
+    ],
+    actions: [],
+    reactions: [],
+  },
+  {
+    service_id: 6,
+    name: "Youtube",
+    status: true,
+    icon: "https://img.freepik.com/vecteurs-premium/logo-youtube-rouge-logo-medias-sociaux_197792-1803.jpg?w=360",
+    buttons: [
+      {
+        name: "linked",
+        path: "",
+      },
+    ],
+    actions: [
+      {
+        action_id: 5,
+        name: "video_uploaded",
+      },
+      {
+        action_id: 6,
+        name: "video_liked",
+      },
+    ],
+    reactions: [
+      {
+        reaction_id: 6,
+        name: "comment_video",
+      },
+      {
+        reaction_id: 7,
+        name: "like_video",
+      },
+      {
+        reaction_id: 8,
+        name: "share_video",
+      },
+    ],
+  },
+  {
+    service_id: 7,
+    name: "Reddit",
+    status: false,
+    icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ0neIueoYcxj62azhnFSpTxZFdQMcITEjCw&s",
+    buttons: [
+      {
+        name: "not_linked",
+        path: "",
+      },
+    ],
+    actions: [],
+    reactions: [],
+  },
+  {
+    service_id: 8,
+    name: "Google",
+    status: false,
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png",
+    buttons: [
+      {
+        name: "google_auth",
+        path: "http://localhost:4242/api/auth",
+      },
+    ],
+    actions: [],
+    reactions: [],
+  },
+  {
+    service_id: 9,
+    name: "Facebook",
+    status: false,
+    icon: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Facebook_logo_%28square%29.png",
+    buttons: [
+      {
+        name: "not_linked",
+        path: "",
+      },
+    ],
+    actions: [],
+    reactions: [],
+  },
+  {
+    service_id: 10,
+    name: "Instagram",
+    status: true,
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/768px-Instagram_icon.png",
+    buttons: [
+      {
+        name: "linked",
+        path: "",
+      },
+    ],
+    actions: [
+      {
+        action_id: 11,
+        name: "receive_private_message",
+      },
+      {
+        action_id: 12,
+        name: "receive_friend_request",
+      },
+      {
+        action_id: 13,
+        name: "post_like",
+      },
+      {
+        action_id: 14,
+        name: "story_seen",
+      },
+    ],
+    reactions: [
+      {
+        reaction_id: 13,
+        name: "send_private_message",
+      },
+      {
+        reaction_id: 14,
+        name: "send_friend_request",
+      },
+      {
+        reaction_id: 15,
+        name: "like_post",
+      },
+      {
+        reaction_id: 16,
+        name: "share_story",
+      },
+    ],
+  },
 ];
 
-
 /* ----- URLS ----- */
-const baseUrl = 'http://localhost:3000/api/tips'; // TODO: Set services url
-
+const baseUrl = "http://localhost:3000/api/tips"; // TODO: Set services url
 
 /* ----- FETCH ----- */
 export async function fetchServices() {
-    // try {
-    //     const response = await fetchGet(baseUrl);
-    //     const jsonResponse = await response.json();
-    //     services.clear();
-    //     lastFetch = Date.now();
-    //     for (let i = 0; i < jsonResponse.length; i++) {
-    //         const tmp = { ...jsonResponse[i] as IService };
-    //         services.set(tmp.service_id, tmp);
-    //     }
-    // } catch (error) {
-    //     console.error("Error fetching services: ", error);
-    // }
+  // try {
+  //     const response = await fetchGet(baseUrl);
+  //     const jsonResponse = await response.json();
+  //     services.clear();
+  //     lastFetch = Date.now();
+  //     for (let i = 0; i < jsonResponse.length; i++) {
+  //         const tmp = { ...jsonResponse[i] as IService };
+  //         services.set(tmp.service_id, tmp);
+  //     }
+  // } catch (error) {
+  //     console.error("Error fetching services: ", error);
+  // }
 
-    services.clear();
-    lastFetch = Date.now();
-    for (let i = 0; i < tmp.length; i++) {
-        const tmpService = { ...tmp[i] as IService };
-        services.set(tmpService.service_id, tmpService);
-    }
+  services.clear();
+  lastFetch = Date.now();
+  for (let i = 0; i < tmp.length; i++) {
+    const tmpService = { ...(tmp[i] as IService) };
+    services.set(tmpService.service_id, tmpService);
+  }
 }
-
 
 /* ----- GETTERS ----- */
 export async function getServices() {
-    if (services.size === 0 || Date.now() - lastFetch > 1000 * 60 * 60 * 24)
-        await fetchServices();
-    return services;
+  if (services.size === 0 || Date.now() - lastFetch > 1000 * 60 * 60 * 24) await fetchServices();
+  return services;
 }
-
