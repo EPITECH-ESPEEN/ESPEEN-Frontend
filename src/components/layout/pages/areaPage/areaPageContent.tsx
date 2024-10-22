d203b87f69f23db6e762de9bfb7ce684a5e869d1
dragusheen <nathan.tirolf@epitech.eu>
2024-10-22 14:00:19 +0200

    [+] proximity connection

--- src/components/layout/pages/areaPage/areaPageContent.tsx
+++ src/components/layout/pages/areaPage/areaPageContent.tsx
@@ -20,7 +20,7 @@ import {
 import '@xyflow/react/dist/style.css';
 import { initialEdges, initialNodes, nodesIds, nodeTypes, setInitialEdges, setInitialNodes } from "src/store/Nodes";
 import Sidebar from "./sidebar";
-import { INode } from "src/types/Node";
+import { IEdge, INode } from "src/types/Node";
 import { getNodeGraph, stringifyGraph } from "src/services/nodes";
 import Modal from "src/components/modal/default/modal";
 import { useTranslation } from "react-i18next";
@@ -37,12 +37,90 @@ const AreaPageContent: React.FC = () => {
     const { screenToFlowPosition } = useReactFlow();
     const { t } = useTranslation();
 
+
+
+
+
+
+
+
+    const MIN_DISTANCE = 600;
+
+    const getClosestEdge = (node: INode, nodes: INode[]): IEdge | null => {
+        let closestNode: INode | null = null;
+        let closestDistance = Number.MAX_VALUE;
+
+        for (const n of nodes) {
+            if (n.id === node.id) continue;
+
+            const dx = n.position.x - node.position.x;
+            const dy = n.position.y - node.position.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance < closestDistance && distance < MIN_DISTANCE) {
+                closestDistance = distance;
+                closestNode = n;
+            }
+        }
+        if (!closestNode)
+            return null;
+        const isSource = closestNode.position.x < node.position.x;
+        return {
+            id: isSource
+                ? `${closestNode.id}-${node.id}`
+                : `${node.id}-${closestNode.id}`,
+            type: 'default',
+            source: isSource ? closestNode.id : node.id,
+            target: isSource ? node.id : closestNode.id,
+        };
+    };
+
+    const onNodeDrag = useCallback(
+        (_: any, node: INode) => {
+            const closeEdge = getClosestEdge(node, nodes);
+            setEdges((existingEdges) => {
+                const updatedEdges = existingEdges.filter((e) => e.type !== 'default');
+                if (closeEdge && !updatedEdges.some((e) => e.source === closeEdge.source && e.target === closeEdge.target)) {
+                    closeEdge.type = 'default';
+                    updatedEdges.push(closeEdge);
+                }
+                return updatedEdges;
+            });
+        },
+        [nodes, setEdges],
+    );
+
+    const onNodeDragStop = useCallback(
+        (_: any, node: INode) => {
+            const closeEdge = getClosestEdge(node, nodes);
+            setEdges((existingEdges) => {
+                const updatedEdges = existingEdges.filter((e) => e.type !== 'default');
+                if (closeEdge && !updatedEdges.some((e) => e.source === closeEdge.source && e.target === closeEdge.target)) {
+                    updatedEdges.push({
+                        ...closeEdge,
+                        type: 'simplebezier',
+                    });
+                }
+                return updatedEdges;
+            });
+        },
+        [nodes, setEdges],
+    );
+
+
+
+
+
+
+
+
+
+
     const onConnect = useCallback(
         (params: any) => {
             for (const edge of edges)
                 if (edge.target === params.target)
                     return;
-            const newEdge = { ...params, type: 'smoothstep' };
+            const newEdge = { ...params, type: 'simplebezier' };
             setEdges((eds) => addEdge(newEdge, eds));
         },
         [edges, setEdges],
@@ -104,6 +182,8 @@ const AreaPageContent: React.FC = () => {
                     onEdgesChange={onEdgesChange}
                     onConnect={onConnect}
                     nodeTypes={nodeTypes}
+                    onNodeDrag={onNodeDrag}
+                    onNodeDragStop={onNodeDragStop}
                 >
                     <Background gap={12} size={1} />
                 </ReactFlow>
