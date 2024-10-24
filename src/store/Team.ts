e3792a95e167bcca880a58dda02c06c43ac0a9c1
dragusheen <nathan.tirolf@epitech.eu>
2024-10-24 10:52:03 +0200

    [~] upd member pictures

--- src/store/Team.ts
+++ src/store/Team.ts
@@ -15,6 +15,6 @@ export const teamMembers: ITeamMember[] = [
     { name: "Caroline Boilly", role: "caroline_boilly", picture: "/img/caroline_boilly.jpeg" },
     { name: "Clement Piasco", role: "clement_piasco", picture: "/img/clement_piasco.jpeg" },
     { name: "Daniil Stepanov", role: "daniil_stepanov", picture: "/img/daniil_stepanov.jpeg" },
-    { name: "Léonard OURSEL", role: "leonard_oursel", picture: "/img/leonard_oursel.jpeg" },
-    { name: "Nathan TIROLF", role: "nathan_tirolf", picture: "/img/nathan_tirolf.jpeg" },
+    { name: "Léonard OURSEL", role: "leonard_oursel", picture: "/img/leonard_oursel.jpg" },
+    { name: "Nathan TIROLF", role: "nathan_tirolf", picture: "/img/nathan_tirolf.png" },
 ];
