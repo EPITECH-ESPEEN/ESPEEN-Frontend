ffcfb012564cb8c89736d4967868f71ea75ae411
dragusheen <nathan.tirolf@epitech.eu>
2024-10-25 17:27:27 +0200

    [+] I18nTranslation + Pictures

--- src/store/Team.ts
+++ src/store/Team.ts
@@ -12,8 +12,8 @@ import { ITeamMember } from "src/types/Team";
 
 /* ----- DATAS ----- */
 export const teamMembers: ITeamMember[] = [
-    { name: "Caroline Boilly", role: "caroline_boilly", picture: "/img/caroline_boilly.jpeg" },
-    { name: "Clement Piasco", role: "clement_piasco", picture: "/img/clement_piasco.jpeg" },
+    { name: "Caroline Boilly", role: "caroline_boilly", picture: "/img/caroline_boilly.jpg" },
+    { name: "Clement Piasco", role: "clement_piasco", picture: "/img/clement_piasco.jpg" },
     { name: "Daniil Stepanov", role: "daniil_stepanov", picture: "/img/daniil_stepanov.jpeg" },
     { name: "Léonard OURSEL", role: "leonard_oursel", picture: "/img/leonard_oursel.jpg" },
     { name: "Nathan TIROLF", role: "nathan_tirolf", picture: "/img/nathan_tirolf.png" },
